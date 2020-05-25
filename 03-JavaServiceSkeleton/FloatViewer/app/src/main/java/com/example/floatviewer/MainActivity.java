a0468b5bdf8c4e70b48bafc8836748bf5d3e3329
Vadym Mishchuk <superoriginalmailboxname@gmail.com>
2020-05-25 23:27:39 +0300

    03-JavaServiceSkeleton: add serviceUnbind() call to float viewer app
    Signed-off-by: Vadym Mishchuk <superoriginalmailboxname@gmail.com>

--- 03-JavaServiceSkeleton/FloatViewer/app/src/main/java/com/example/floatviewer/MainActivity.java
+++ 03-JavaServiceSkeleton/FloatViewer/app/src/main/java/com/example/floatviewer/MainActivity.java
@@ -79,4 +79,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    protected void onDestroy () {
+        super.onDestroy();
+        unbindService(mConnection);
+    }
 }
