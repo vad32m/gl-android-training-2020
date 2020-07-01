d638bfb1d4c0a9f68337a53bc57605fbc31b3110
Vadym Mishchuk <superoriginalmailboxname@gmail.com>
2020-07-01 23:41:46 +0300

    Rework app makefile so it builds without errors
    Signed-off-by: Vadym Mishchuk <superoriginalmailboxname@gmail.com>

--- Android.mk
+++ Android.mk
@@ -4,21 +4,20 @@ include ${CLEAR_VARS}
 
 LOCAL_PACKAGE_NAME := FloatWriter
 
-LOCAL_SRC_FILES := \
-	$(call all-java-files-under, java) \
-	$(call all-Iaidl-files-under, aidl) \
+LOCAL_MODULE_TAGS := optional
 
-LOCAL_USE_APPT2 := true
+LOCAL_SRC_FILES := $(call all-java-files-under, java)
+LOCAL_SRC_FILES += $(call all-Iaidl-files-under, aidl)
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+	android-support-design \
+	android-support-constraint-layout \
+	android-support-v7-appcompat
+
+LOCAL_SDK_VERSION := current
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-LOCAL_STATIC_ANDROID_LIBRARIES := \
-	android-support-v7-appcompat \
-	android-support-constraint-layout
-
-LOCAL_STATIC_JAVA_LIBRARIES += \
-	android-support-constraint-layout-solver
-
-LOCAL_SDK_VERSION := 28
+LOCAL_USE_AAPT2 := true
 
 include ${BUILD_PACKAGE}
