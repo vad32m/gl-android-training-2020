81e0f01b399d8cf926aeb726e599f7f51986e4f8
Vadym Mishchuk <superoriginalmailboxname@gmail.com>
2020-07-13 23:10:39 +0300

    Add HAL and manifests to the build system
    Signed-off-by: Vadym Mishchuk <superoriginalmailboxname@gmail.com>

--- vadym-vendor.mk
+++ vadym-vendor.mk
@@ -1,2 +1,12 @@
 PRODUCT_PACKAGES += FloatExchangeService \
+	vendor.vadym.hardware.LedHal@1.0-service \
 	FloatWriter
+
+DEVICE_MANIFEST_FILE += \
+	vendor/vadym/hardware/interfaces/manifest.xml
+
+DEVICE_MATRIX_FILE += \
+	vendor/vadym/hardware/interfaces/compatibility_matrix.xml
+
+BOARD_SEPOLICY_DIRS += \
+	vendor/vadym/sepolicy
